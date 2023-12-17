659a3fb7945dbd8798f6e99d541379fd9a083832
Abdullah Mahmoud <120322562+abdullahmohs@users.noreply.github.com>
2023-12-17 03:13:34 +0200

    Add files via upload

---
+++ N_Queen/src/n_queen/N_Queen.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package n_queen;
+
+/**
+ *
+ * @author Administrator
+ */
+public class N_Queen {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        GUI g = new GUI();
+        //design d =new design();
+//        solving s=new solving();
+//        s.solveNQwiththreads(4);
+        //print_solution sss = new print_solution();
+    }
+
+}
